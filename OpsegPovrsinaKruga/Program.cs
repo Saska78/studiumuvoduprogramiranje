896885a92d103eeac46454d92480c52de83533d6
Bojan <Bojan@DESKTOP-7I4C9H9>
2024-12-22 01:00:35 +0100

    Add project files.

---
+++ OpsegPovrsinaKruga/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Program racuna opseg i povrsinu kruga, a trazi unos radijusa.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsegPovrsinaKruga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite radijus:");
+            float r = float.Parse(Console.ReadLine());
+            const float pi = 3.14f;
+            float P, O;
+            O = 2 * r * pi;
+            P = r * r * pi;//P = r  * O /2;
+            Console.WriteLine("Povrsina je "+P+"\nOpseg je"+O);
+            Console.ReadKey();
+
+
+                
+        }
+    }
+}
